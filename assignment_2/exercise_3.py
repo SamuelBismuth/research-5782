24763a4bc86b58792cf3889c677e38d0bc959f2b
Samuel <sam101@hotmail.fr>
2022-03-08 17:26:19 +0200

    Adding setitem

--- assignment_2/exercise_3.py
+++ assignment_2/exercise_3.py
@@ -7,16 +7,31 @@ class List(list):
         If a tupple is given as an argument, handle the case in this function.
         Otherwise, act as a regular list.
         '''
-        if type(key) is tuple:
-            self_copy = self.copy()
+        string_exec = 'self'
+        if hasattr(key, '__iter__'):
             for item in key:
-                if type(self_copy) is int or item >= len(self_copy):
-                    # Raise an exception.
-                    raise Exception("Check the arguments.")
-                else:
-                    self_copy = self_copy[item]
-            return self_copy
-        return list(self)[key]
+                string_exec = string_exec + '[{0}]'.format(item)
+        else:
+            return list(self)[key]
+        return eval(string_exec)
+
+    
+    def __setitem__(self, key, value):
+        '''
+        Overide the __setitem__ list function which is called when using brackets "[]".
+        If a tupple is given as an argument, handle the case in this function.
+        Otherwise, act as a regular list.
+        '''       
+        string_exec = 'self'
+        if hasattr(key, '__iter__'):
+            for item in key:
+                string_exec = string_exec + '[{0}]'.format(item)
+            string_exec = string_exec + ' = {0}'.format(value)
+            exec(string_exec)
+        else:
+            self.pop(key)
+            self.insert(key, value)
+       
 
 # Example of List (which is not a regular python list).   
 mylist = List([
@@ -31,18 +46,33 @@ Running examples
 
 if __name__ == '__main__':
     print(mylist[0])
+    mylist.append(5)
+    print(mylist)
     print(mylist[0, 1, 3])
     print(mylist[0, 1])
     print(mylist[0])
     print(mylist[1])
     print(mylist)
+    mylist[0] = 4
+    print(mylist)
+    mylist[0] = 9
+    print(mylist)
+    mylist[1] = 9
+    print(mylist)
+    mylist[2, 1] = 3
+    print(mylist)
+
+    try:
+        mylist[0, 1, 3, 4] = 3
+    except Exception as error:
+        print(error)
 
     try:
         print(mylist[10, 10])
     except Exception as error:
-        print('error')
+        print(error)
         
     try:
         print(mylist[0, 1, 3, 4])
     except Exception as error:
-        print('error')
+        print(error)
